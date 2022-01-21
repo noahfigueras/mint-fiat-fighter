6891a66f37928a399787ee6df2b16b77e7c0ec43
noah figueras <noahfigueras@gmail.com>
2022-01-21 15:45:48 -0800

    added wallets + downgrade react-scripts version

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,12 +1,41 @@
 import './Login.css';
 import logo from '../../img/logo2.png';
-import metamask from '../../img/logo-metamask.png';
+import logoMetamask from '../../img/logo-metamask.png';
 import QR from '../../img/QR.png';
 import email from '../../img/email.png';
 import lightbolt from '../../img/lightbolt.png';
 import info from '../../img/info.png';
+import { ethers } from "ethers";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const Login = () => {
+	let Provider;
+
+	function setProvider(provider) {
+		Provider = new ethers.providers.Web3Provider(provider);
+	}
+
+	async function walletConnect() {
+		const provider = new WalletConnectProvider({
+			infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+		});
+		try{
+			await provider.enable();
+			setProvider(provider);
+		} catch(err) {
+			console.log(err);
+		}
+	}
+
+	function metamask() {
+		if(window.ethereum) {
+			const provider = window.ethereum;	
+			setProvider(provider);
+		} else {
+			alert("Please Download metamask");
+		}
+	}
+
   return (
     <div className="landing-page">
       <div className="image">
@@ -15,11 +44,11 @@ const Login = () => {
       <div className="login">
         <img alt='logo' src={logo}/>
         <div className="buttons">
-          <div className="metamask">
-            <img alt='metamask' src={metamask}/> 
+          <div onClick={metamask} className="metamask">
+            <img alt='metamask' src={logoMetamask}/> 
             <p>Login with Metamask</p>
           </div>
-          <div className="other-wallet">
+          <div onClick={walletConnect} className="other-wallet">
             <img alt='walletConnect' src={QR}/> 
             <p>Login with QR Code</p>
           </div>
