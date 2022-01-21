443aa767caed54b03960dc703c13fba10396697c
noah figueras <noahfigueras@gmail.com>
2022-01-20 20:32:59 -0800

    Implemented Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import twitter from './twitter.png';
 import discord from './discord.png';
+
 import Navbar from './navbar/navbar.js';
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="login" element={<Login/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+const Landing = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,7 +33,7 @@ function App() {
           <div className="mint">
             <p>Mint FiatFighter</p>
             <p>0/0</p>
-            <button className="connect">Connect</button>
+            <Link to="login" className="connect">Connect</Link>
             <div className="social">
              <a className="social-link" href="https://twitter.com" target="_blank">
               <img alt="twitter" src={twitter}/>
@@ -30,4 +49,9 @@ function App() {
   );
 }
 
+const Login = () => {
+  return (
+    <div>Login Page</div>
+  );
+}
 export default App;
